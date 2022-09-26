b74680d478da3aea31b7e1b38fa193b54b3364df
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-26 10:37:10 +0200

    continuo costruzione classe evento

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@ class Event
 {
     public string Title { get; set; }
 
-    public DateTime EventDate = new DateTime(); 
-    public int MaxPeopleForEvent { get; }
-    public int Reservation { get; }
+    public DateTime EventDate { get; private set; }
+    public int MaxPeopleForEvent { get; private set; }
+    public int Reservation { get; private set; }
 
-    public Event(string title, int maxPeopleForEvent, int reservation)
+    public Event(string title, DateTime, int maxPeopleForEvent, int reservation)
     {
         Title = title;
         MaxPeopleForEvent = maxPeopleForEvent;
@@ -17,6 +17,32 @@ class Event
 
     public void bookSeats(int number)
     {
+        switch (number)
+        {
+            case 0:
+                if(MaxPeopleForEvent - number > 0 )
+                {
+                    MaxPeopleForEvent -= number;
+                    Reservation += number;
+                    Console.WriteLine(MaxPeopleForEvent);
+                    Console.WriteLine(Reservation);
+                    Console.WriteLine("Prenotazione effettuata con successo");
+                }
+                break;
+            case 1:
+                if (MaxPeopleForEvent - number <= 0)
+                {
+                    Console.WriteLine("Spiacente l'evento è già al completo");
+                }
+                break;
+            default:
+                Console.WriteLine("Non è più possibile prenotare per questo evento");
+                break;
+        }
+    }
 
+    public void cancelReservation(int number)
+    {
+        
     }
 }
