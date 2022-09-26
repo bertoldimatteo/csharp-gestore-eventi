736d3339717d6429040e438ef96529568d01fa04
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-26 09:49:05 +0200

    strutturazione Milestone 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class Event
+{
+    public string Title { get; set; }
+
+    public DateTime EventDate = new DateTime(); 
+    public int MaxPeopleForEvent { get; }
+    public int Reservation { get; }
+
+    public Event(string title, int maxPeopleForEvent, int reservation)
+    {
+        Title = title;
+        MaxPeopleForEvent = maxPeopleForEvent;
+        Reservation = reservation;
+    }
+
+    public void bookSeats(int number)
+    {
+
+    }
+}
