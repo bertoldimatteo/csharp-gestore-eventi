b228a3c1611ecb7fdfb8f118aad29566cb96252b
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-26 14:27:25 +0200

    aggiunto program event

--- Program.cs
+++ Program.cs
@@ -44,4 +44,3 @@ while (enter)
             break;
     }
 }
-
---
+++ ProgramEvent.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+class ProgramEvent
+{
+    public string Title { get; set; }
+
+    private List<Event> events;
+
+    public ProgramEvent(string title)
+    {
+        Title = title;
+        events = new List<Event>();
+    }
+}
