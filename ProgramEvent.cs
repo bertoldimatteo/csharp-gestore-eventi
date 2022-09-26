10eaadf777dd10c8bca05033126502b51ae942ce
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-09-26 14:44:57 +0200

    in programElement presente aggiunta evento + filtraggio per data + stampo in console

--- ProgramEvent.cs
+++ ProgramEvent.cs
@@ -10,4 +10,30 @@ class ProgramEvent
         Title = title;
         events = new List<Event>();
     }
+
+    public void addEvent(Event el)
+    {
+        events.Add(el);
+    }
+
+    public List<Event> dateEvent(string date)
+    {
+        string[] splitDate = date.Split('/');
+        int day = Convert.ToInt32(splitDate[0]);
+        int month = Convert.ToInt32(splitDate[1]);
+        int year = Convert.ToInt32(splitDate[2]);
+        DateTime eventDate = new DateTime(day, month, year);
+
+        List<Event> result = events.FindAll(e => e.Date == eventDate);
+        return result;
+    }
+
+    public static void printEvents(List<Event> allEvents)
+    {
+        Console.WriteLine("Lista Eventi:");
+        foreach (Event el in allEvents)
+        {
+            Console.WriteLine(el);
+        }
+    }
 }
